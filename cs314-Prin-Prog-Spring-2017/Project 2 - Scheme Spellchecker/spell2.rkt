78f175792e7aeb5d94da55fbc1e35ef1659c81f2
Rafal Stapinski <stapinskirafal@gmail.com>
2017-04-25 11:04:18 -0400

    proj 2 scheme done

---
+++ cs314-Prin-Prog-Spring-2017/Project 2 - Scheme Spellchecker/spell2.rkt
@@ -0,0 +1,219 @@
+#lang racket
+; *********************************************
+; *  314 Principles of Programming Languages  *
+; *  Spring 2017                              *
+; *  Student Version                          *
+; *********************************************
+
+;; contains "ctv", "A", and "reduce" definitions
+(load "include.ss")
+
+;; contains simple dictionary definition
+(load "test-dictionary.ss")
+
+;; -----------------------------------------------------
+;; HELPER FUNCTIONS
+
+;; *** CODE FOR ANY HELPER FUNCTION GOES HERE ***
+
+(define eval_bitvector
+  (lambda (hashfunctionlist dict)
+    (if (null? hashfunctionlist)
+      '()
+      (append
+        (map
+          (lambda (w)
+            ((car hashfunctionlist) w)
+          )
+          dict
+        )
+        (eval_bitvector
+          (cdr hashfunctionlist)
+          dict
+        )
+      )
+    )
+  )
+)
+
+(define contains
+  (lambda (lst element)
+    (cond
+      (
+        (null? lst)
+        1
+      )
+      (
+        (= (car lst) element)
+        0
+      )
+      (else
+        (contains (cdr lst) element)
+      )
+    )
+  )
+)
+
+(define contains_all
+  (lambda (wordhashes bitvector)
+    (if (null? wordhashes)
+      '()
+      (append
+        (list (contains bitvector (car wordhashes)))
+        (contains_all (cdr wordhashes) bitvector)
+      )
+    )
+  )
+)
+
+(define get_result
+  (lambda (bools)
+    (reduce + bools 0 )
+  )
+)
+
+
+;; -----------------------------------------------------
+;; KEY FUNCTION
+
+(define key
+  (lambda (w)
+    (if (null? w)
+      5187
+      (+
+        (*
+          29
+          (key (cdr w))
+        )
+        (ctv (car w))
+      )
+    )
+  )
+)
+
+(define key
+  (lambda (w)
+    (if null? w
+      5187
+      (reduce + (key w)
+        (map
+          (lambda (letter)
+            (ctv letter)
+          )
+          (reverse)
+        )
+      )
+    )
+  )
+)
+
+;; -----------------------------------------------------
+;; EXAMPLE KEY VALUES
+;;   (key '(h e l l o))       = 106402241991
+;;   (key '(m a y))           = 126526810
+;;   (key '(t r e e f r o g)) = 2594908189083745
+
+;; -----------------------------------------------------
+;; HASH FUNCTION GENERATORS
+
+;; value of parameter "size" should be a prime number
+(define gen-hash-division-method
+  (lambda (size) ;; range of values: 0..size-1
+    (lambda (w)
+      (modulo (key w) size)
+    )
+))
+
+
+;; value of parameter "size" is not critical
+;; Note: hash functions may return integer values in "real"
+;;       format, e.g., 17.0 for 17
+
+(define gen-hash-multiplication-method
+  (lambda (size) ;; range of values: 0..size-1
+    (lambda (w)
+      (floor
+        (*
+          size
+          (-
+            (*
+              (key w)
+              A
+            )
+            (floor
+              (*
+                (key w)
+                A
+              )
+            )
+          )
+        )
+      )
+    )
+))
+
+
+;; -----------------------------------------------------
+;; EXAMPLE HASH FUNCTIONS AND HASH FUNCTION LISTS
+
+(define hash-1 (gen-hash-division-method 70111))
+(define hash-2 (gen-hash-division-method 89997))
+(define hash-3 (gen-hash-multiplication-method 7224))
+(define hash-4 (gen-hash-multiplication-method 900))
+
+(define hashfl-1 (list hash-1 hash-2 hash-3 hash-4))
+(define hashfl-2 (list hash-1 hash-3))
+(define hashfl-3 (list hash-2 hash-3))
+
+;; -----------------------------------------------------
+;; EXAMPLE HASH VALUES
+;;   to test your hash function implementation
+;;
+;;  (hash-1 '(h e l l o))       ==> 35616
+;;  (hash-1 '(m a y))           ==> 46566
+;;  (hash-1 '(t r e e f r o g)) ==> 48238
+;;
+;;  (hash-2 '(h e l l o))       ==> 48849
+;;  (hash-2 '(m a y))           ==> 81025
+;;  (hash-2 '(t r e e f r o g)) ==> 16708
+;;
+;;  (hash-3 '(h e l l o))       ==> 6331.0
+;;  (hash-3 '(m a y))           ==> 2456.0
+;;  (hash-3 '(t r e e f r o g)) ==> 1806.0
+;;
+;;  (hash-4 '(h e l l o))       ==> 788.0
+;;  (hash-4 '(m a y))           ==> 306.0
+;;  (hash-4 '(t r e e f r o g)) ==> 225.0
+
+
+;; -----------------------------------------------------
+;; SPELL CHECKER GENERATOR
+
+(define gen-checker
+  (lambda (hashfunctionlist dict)
+    (let ((bitvector (eval_bitvector hashfunctionlist dict)))
+      (lambda (w)
+        (let ((wordhashes (eval_bitvector hashfunctionlist (list w))))
+          (if (= 0 (get_result (contains_all wordhashes bitvector)))
+            #t
+            #f
+          )
+        )
+      )
+    )
+  )
+)
+
+
+;; -----------------------------------------------------
+;; EXAMPLE SPELL CHECKERS
+
+(define checker-1 (gen-checker hashfl-1 dictionary))
+(define checker-2 (gen-checker hashfl-2 dictionary))
+(define checker-3 (gen-checker hashfl-3 dictionary))
+
+;; EXAMPLE APPLICATIONS OF A SPELL CHECKER
+;;
+;;  (checker-1 '(a r g g g g)) ==> #f
+;;  (checker-2 '(h e l l o)) ==> #t
+;;  (checker-2 '(a r g g g g)) ==> #t  // false positive
