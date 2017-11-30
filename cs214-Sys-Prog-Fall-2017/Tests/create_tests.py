31197c4f92b735a6ebd56b1a92a7b2efd9beb378
Rafal Stapinski <stapinskirafal@gmail.com>
2017-11-29 21:26:36 -0500

    create tests file

--- cs214-Sys-Prog-Fall-2017/Tests/create_tests.py
+++ cs214-Sys-Prog-Fall-2017/Tests/create_tests.py
@@ -1,5 +1,45 @@
-from shutil import copyfile
+import os
 
-def create_deep():
+def create_shallow(amount):
 
-    
+    os.mkdir("shallow%d" % amount)
+    os.mkdir("shallow-output-p%d" % amount)
+    os.mkdir("shallow-output-t%d" % amount)
+
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+
+    for i in range(amount):
+
+        os.symlink(
+            '%s/movie_metadata.csv' % (base_dir),
+            'shallow%d/%d.csv' % (amount, i)
+        )
+
+def create_deep(amount):
+
+    working_dir = "deep%d/" % amount
+
+    os.mkdir(working_dir)
+    os.mkdir("deep-output-p%d" % amount)
+    os.mkdir("deep-output-t%d" % amount)
+
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+
+    for i in range(amount):
+
+        working_dir += '%d/' % i
+
+        os.mkdir(working_dir)
+        os.symlink(
+            '%s/movie_metadata.csv' % (base_dir),
+            '%s/%d.csv' % (working_dir, i)
+        )
+
+
+# create_shallow(1)
+# create_shallow(10)
+# create_shallow(100)
+# create_shallow(1000)
+
+create_deep(1)
+create_deep(10)
