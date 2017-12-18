9715e779a227a19625dd944d6468dd6f9d9b2208
Rafal Stapinski <stapinskirafal@gmail.com>
2017-12-18 16:07:43 -0500

    remove mike CA

--- cs211-Comp-Arch-Fall-2017/project1/eighth/eighth.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-struct Node
-{
-    int value;
-    int level;
-    struct Node *leftChild;
-    struct Node *rightChild;
-};
--- cs211-Comp-Arch-Fall-2017/project1/second/second.h
+++
@@ -1,4 +0,0 @@
-struct Node {
-  int value;
-  struct Node *next;
-};
