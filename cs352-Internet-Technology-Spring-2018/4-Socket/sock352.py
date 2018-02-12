4a932cb55ff5a49030e7bf889ef7557d4be25f47
Rafal Stapinski <stapinskirafal@gmail.com>
2018-02-11 19:56:52 -0500

    blank socket

---
+++ cs352-Internet-Technology-Spring-2018/4-Socket/sock352.py
@@ -0,0 +1,21 @@
+import socket as ip
+
+class socket:
+
+    def __init__(self):
+        pass
+
+    def socket(self):
+        pass
+
+    def bind(self, address):
+        pass
+
+    def sendto(self, buffer, address):
+        pass
+
+    def recvfrom(self, nbytes):
+        pass
+
+    def close(self):
+        pass
